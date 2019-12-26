619ddd5f8f57c7a4cdcb29af7da9db4057cda270
Nshan <nshan.potikyan@gmail.com>
2019-12-26 18:04:58 +0200

    Adding 3 other clustering algorithms

--- GeneticClus.py
+++ GeneticClus.py
@@ -1,10 +1,9 @@
 import random 
-from sklearn.cluster import KMeans
-from sklearn.cluster import MeanShift
-from sklearn.cluster import DBSCAN 
+from sklearn.cluster import KMeans, MeanShift, DBSCAN, \
+    AffinityPropagation, SpectralClustering, AgglomerativeClustering
 from sklearn import metrics
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import csv
 import pygmo as pg
 from scipy.stats import spearmanr
@@ -27,7 +26,7 @@ class kmeans:
         p = self.generate_pop(size=1)[0]
         L = len(self.params)
         pos = random.randint(0,L-1)
-        
+
         if pos<=3:
             pop[1].n_init= p[1].n_init
         if pos<=2:
@@ -36,14 +35,14 @@ class kmeans:
             pop[1].algorithm= p[1].algorithm
         if pos==0:
             pop[1].n_clusters= p[1].n_clusters
-   
+
         return pop
 
-    def cross_over(self,pop1,pop2):
+    def cross_over(self,pop,pop2):
         p = pop2
         L = len(self.params)
         pos = random.randint(0,L-1)
-        
+
         if pos<=3:
             pop[1].n_init= p[1].n_init
         if pos<=2:
@@ -52,7 +51,7 @@ class kmeans:
             pop[1].algorithm= p[1].algorithm
         if pos==0:
             pop[1].n_clusters= p[1].n_clusters
-   
+
         return pop
 
 class meanshift:
@@ -73,7 +72,7 @@ class meanshift:
         p = self.generate_pop(size=1)[0]
         L = len(self.params)
         pos = random.randint(0,L-1)
-        
+
         if pos<=3:
             pop[1].max_iter= p[1].max_iter
         if pos<=2:
@@ -82,14 +81,14 @@ class meanshift:
             pop[1].bin_seeding= p[1].bin_seeding
         if pos==0:
             pop[1].cluster_all= p[1].cluster_all
-   
+
         return pop
 
     def cross_over(self,pop,pop2):
         p = pop2
         L = len(self.params)
         pos = random.randint(0,L-1)
-        
+
         if pos<=3:
             pop[1].max_iter= p[1].max_iter
         if pos<=2:
@@ -98,7 +97,7 @@ class meanshift:
             pop[1].bin_seeding= p[1].bin_seeding
         if pos==0:
             pop[1].cluster_all= p[1].cluster_all
-   
+
         return pop
 
 class dbscan:
@@ -133,7 +132,7 @@ class dbscan:
             pop[1].leaf_size= p[1].leaf_size
         if pos==0:
             pop[1].p= p[1].p
-   
+
         return pop
 
     def mutate(self,pop,pop2):
@@ -152,85 +151,260 @@ class dbscan:
             pop[1].leaf_size= p[1].leaf_size
         if pos==0:
             pop[1].p= p[1].p
-   
+
         return pop
 
 
+class AffinPropagation:
+    """
+    Affinity Propagation
+    """
+    def __init__(self):
+        self.params = ["damping", "max_iter", "affinity"]
+        self.L = len(self.params)  # number of parameters
+
+    @staticmethod
+    def generate_pop(size=1):
+        population = []
+        for i in range(size+1):
+            damping = random.uniform(0.5, 1)
+            max_iter = random.randint(100, 300)
+            affinity = random.choice(['euclidean', 'precomputed'])
+            population.append(["affinity_propagation",
+                               AffinityPropagation(damping=damping, max_iter=max_iter, affinity=affinity)])
+        return population
+
+    def mutate(self, pop):
+        p = self.generate_pop(size=1)[0]
+        L = self.L
+        pos = random.randint(0, L-1)
+
+        if pos <= 2:
+            pop[1].damping = p[1].damping
+        if pos <= 1:
+            pop[1].max_iter = p[1].max_iter
+        if pos == 0:
+            pop[1].affinity = p[1].affinity
+
+        return pop
+
+    def cross_over(self, pop, pop2):
+        p = pop2
+        L = self.L
+        pos = random.randint(0, L-1)
+
+        if pos <= 2:
+            pop[1].damping = p[1].damping
+        if pos <= 1:
+            pop[1].max_iter = p[1].max_iter
+        if pos == 0:
+            pop[1].affinity = p[1].affinity
+
+        return pop
 
 
-class autoclus:
-        def __init__(self,dfile=""):
-            data = pd.read_csv(dfile, header=None,na_values='?')           
-            self.data = data
+class SpectralCluster:
+    """
+    Spectral Clustering
+    """
+    def __init__(self):
+        self.params = ["n_clusters", "eigen_solver",
+                       "n_init", "gamma", "affinity"]
+        self.L = len(self.params)  # number of parameters
 
-            self.kmeans=kmeans()
-            self.meanshift=meanshift()
-            self.dbscan=dbscan()
+    @staticmethod
+    def generate_pop(size=1):
+        population = []
+        for i in range(size+1):
+            n_clusters = random.randint(2, 100)
+            eigen_solver = random.choice([None, 'arpack', 'lobpcg', 'amg'])
+            n_init = random.randint(1, 20)
+            gamma = random.uniform(0.5, 3)
+            affinity = random.choice(['nearest_neighbors', 'rbf'])
+            population.append(["spectral_clustering",
+                               SpectralClustering(n_clusters=n_clusters,
+                                                  eigen_solver=eigen_solver,
+                                                  n_init=n_init,
+                                                  gamma=gamma,
+                                                  affinity=affinity)])
+        return population
 
-            self.population=[]
-        
-        def generate_pop(self,size=30):
-            p=int(size/3)
-            k=size-(2*p)
-            population=[]
-            population.extend(self.kmeans.generate_pop(k))
-            population.extend(self.meanshift.generate_pop(p))
-            population.extend(self.dbscan.generate_pop(p))
+    def mutate(self, pop):
+        p = self.generate_pop(size=1)[0]
+        L = self.L
+        pos = random.randint(0, L-1)
 
-            self.population = population
-            return population
-            
-        def evaluate_pop(self):
-            
-            new_population=[]
-            vals12=[]
-            vals3=[]
-            indx=[]
-            for i in range(len(self.population)):
-                Metrics={}
-                try: 
-                    clustering = self.population[i][1].fit(self.data) 
-                except:
-                    continue
+        if pos <= 4:
+            pop[1].n_clusters = p[1].n_clusters
+        if pos <= 3:
+            pop[1].eigen_solver = p[1].eigen_solver
+        if pos <= 2:
+            pop[1].n_init = p[1].n_init
+        if pos <= 1:
+            pop[1].gamma = p[1].gamma
+        if pos == 0:
+            pop[1].affinity = p[1].affinity
 
-                if len(set(list(clustering.labels_)))==1:
-                    continue
-                try:
-                    sample_size=int(len(self.data)*0.1)
-                    if sample_size<100:
-                        sample_size=len(self.data)
-                    Metrics["davies_bouldin_score"]=metrics.davies_bouldin_score(self.data,  clustering.labels_) 
-                    Metrics["silhouette_score"] = metrics.silhouette_score(self.data, clustering.labels_, metric='euclidean', sample_size=sample_size,random_state=0)
-                    Metrics["calinski_harabasz_score"]= metrics.calinski_harabasz_score(self.data,  clustering.labels_) 
-                except:
-                    continue
-                indx.append(i)
-                vals12.append([Metrics["davies_bouldin_score"],Metrics["silhouette_score"]])
-                vals3.append(Metrics["calinski_harabasz_score"])
+        return pop
 
-            ndf, dl, dc, ndr = pg.fast_non_dominated_sorting(points =vals12)
-            ndf.reverse()
-            
-            print(ndf)
-        def cross_over(self,pop1,pop2):
-            if pop1[0] != pop2[0]:
-                return None
-            else:
-                model=eval("self."+pop1[0])
-                n_pop = model.mutate(pop1,pop2)
-                return n_pop
+    def cross_over(self, pop, pop2):
+        p = pop2
+        L = self.L
+        pos = random.randint(0, L-1)
 
-        def mutation(self,population=[]):
-            new_population=[]
-            for pop in population:
-                model=eval("self."+pop[0])
-                n_pop = model.mutate(pop)
-                new_population.append(n_pop)
+        if pos <= 4:
+            pop[1].n_clusters = p[1].n_clusters
+        if pos <= 3:
+            pop[1].eigen_solver = p[1].eigen_solver
+        if pos <= 2:
+            pop[1].n_init = p[1].n_init
+        if pos <= 1:
+            pop[1].gamma = p[1].gamma
+        if pos == 0:
+            pop[1].affinity = p[1].affinity
 
-            return new_population 
+        return pop
 
 
+class AgglomerativeCluster:
+    """
+    Agglomerative Clustering
+    """
+    def __init__(self):
+        self.params = ["n_clusters", "linkage", "affinity"]
+        self.L = len(self.params)  # number of parameters
 
-auto = autoclus(dfile="test.csv")
+    @staticmethod
+    def generate_pop(size=1):
+        population = []
+        for i in range(size+1):
+            n_clusters = random.randint(2, 100)
+            linkage = random.choice(['ward', 'complete', 'average', 'single'])
+            affinity = random.choice(['euclidean', 'l1', 'l2',
+                                      'manhattan', 'cosine'])
+            population.append(["agglomerative_clustering",
+                               AgglomerativeClustering(n_clusters=n_clusters,
+                                                       linkage=linkage,
+                                                       affinity=affinity)])
+        return population
+
+    def mutate(self, pop):
+        p = self.generate_pop(size=1)[0]
+        L = self.L
+        pos = random.randint(0, L-1)
+
+        if pos <= 2:
+            pop[1].n_clusters = p[1].n_clusters
+        if pos <= 1:
+            pop[1].linkage = p[1].linkage
+        if pos == 0:
+            pop[1].affinity = p[1].affinity
+
+        return pop
+
+    def cross_over(self, pop, pop2):
+        p = pop2
+        L = self.L
+        pos = random.randint(0, L-1)
+
+        if pos <= 2:
+            pop[1].n_clusters = p[1].n_clusters
+        if pos <= 1:
+            pop[1].linkage = p[1].linkage
+        if pos == 0:
+            pop[1].affinity = p[1].affinity
+
+        return pop
+
+
+class AutoClus:
+
+    def __init__(self, dfile=""):
+        data = pd.read_csv(dfile, header=None, na_values='?')
+        self.data = data
+
+        self.kmeans = kmeans()
+        self.meanshift = meanshift()
+        self.dbscan = dbscan()
+        self.affinity_propagation = AffinPropagation()
+        self.spectral_clustering = SpectralCluster()
+        self.agglomerative_clustering = AgglomerativeCluster()
+
+        self.population = []
+
+    def generate_pop(self, size=30):
+        """
+
+        :param size: size needs to be divisible by # of clustering algorithms
+        :return:
+        """
+        nr_algorithms = 6# number of clustering algorithms
+        p = int(size / nr_algorithms)
+        population = []
+        population.extend(self.kmeans.generate_pop(p))
+        population.extend(self.meanshift.generate_pop(p))
+        population.extend(self.dbscan.generate_pop(p))
+        population.extend(self.affinity_propagation.generate_pop(p))
+        population.extend(self.spectral_clustering.generate_pop(p))
+        population.extend(self.agglomerative_clustering.generate_pop(p))
+
+        self.population = population
+        return population
+
+    def evaluate_pop(self):
+
+        new_population = []
+        vals12 = []
+        vals3 = []
+        indx = []
+        for i in range(len(self.population)):
+            Metrics = {}
+            try:
+                clustering = self.population[i][1].fit(self.data)
+            except:
+                continue
+
+            if len(set(list(clustering.labels_))) == 1:
+                continue
+            try:
+                sample_size = int(len(self.data)*0.1)
+                if sample_size < 100:
+                    sample_size = len(self.data)
+                Metrics["davies_bouldin_score"] = metrics.davies_bouldin_score(self.data,  clustering.labels_)
+                Metrics["silhouette_score"] = metrics.silhouette_score(self.data, clustering.labels_,
+                                                                       metric='euclidean', sample_size=sample_size,
+                                                                       random_state=0)
+                Metrics["calinski_harabasz_score"] = metrics.calinski_harabasz_score(self.data,  clustering.labels_)
+            except:
+                continue
+            indx.append(i)
+            vals12.append([Metrics["davies_bouldin_score"], Metrics["silhouette_score"]])
+            vals3.append(Metrics["calinski_harabasz_score"])
+
+        ndf, dl, dc, ndr = pg.fast_non_dominated_sorting(points=vals12)
+        ndf.reverse()
+
+        print(ndf)
+
+    def cross_over(self, pop1, pop2):
+        if pop1[0] != pop2[0]:
+            return None
+        else:
+            model = eval("self."+pop1[0])
+            n_pop = model.mutate(pop1, pop2)
+            return n_pop
+
+    def mutation(self, population=[]):
+        new_population = []
+        for pop in population:
+            model = eval("self."+pop[0])
+            n_pop = model.mutate(pop)
+            new_population.append(n_pop)
+
+        return new_population
+
+
+
+auto = AutoClus(dfile="test.csv")
 auto.generate_pop(15)
 auto.evaluate_pop()
