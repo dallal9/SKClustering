7390e375637b9069fa0110ec26177485c659dbdd
Abdelrhman Eldallal <abdelrhman.d@aucegypt.edu>
2020-01-18 21:58:09 +0200

    add genetic code evaluation script

---
+++ EvalGen.py
@@ -0,0 +1,42 @@
+import time 
+from GeneticClus import AutoClus
+from sklearn import metrics
+import glob
+import pandas as pd
+
+path="./Datasets/processed/"
+allFiles = glob.glob(path + "*.csv")
+'''
+create file  for cvis
+'''
+'''
+save output
+'''
+
+for file_name in allFiles:
+    data = pd.read_csv(file_name, header=None,na_values='?')
+    y = data.iloc[:,-1]              
+    data = data.iloc[:, :-1]
+
+    t1=time.time()
+    auto = AutoClus(dfile=file_name,
+        y=True ,
+        cvi1=["i_index",1],
+        cvi2=["ratkowsky_lance",1],
+        cvi3=["banfeld_raferty",-1],
+        size=50,
+        iterations=10) #initialize class object
+    #top_20 = auto.evaluate_pop() #evaluate population and return top 20% after n iterations 
+    # print((time.time()-t1))
+    # print(auto.scores)
+
+    auto.size=8
+    auto.population=[]
+    models = auto.generate_pop(population=[])
+    for model in models:
+        try:
+            model[1].fit(data)
+            nmi=metrics.normalized_mutual_info_score(y ,model[1].labels_)
+        except:
+            pass
+        print(nmi)
